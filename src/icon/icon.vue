033f168bf4ef653564d5ecf61eb84c366dbf5753
shunyue1320 <1277952981@qq.com>
2020-10-29 21:19:49 +0800

    引入阿里svg图标

---
+++ src/icon/icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="u-icon">
+    <use :xlink:href="`#icon-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+  import './svg'
+
+  export default {
+    name: 'UabIcon',
+    props: [ 'name' ]
+  }
+</script>
+
+<style lang="scss" scoped>
+  .u-icon {
+    width: 1em;
+    height: 1em;
+    fill:#fff;
+  }
+</style>
