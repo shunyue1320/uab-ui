cf47edf33b4e6f34c7821f5c9a39fcd5fe955cd8
shunyue1320 <1277952981@qq.com>
2020-11-01 20:59:42 +0800

    vueperss button组件

---
+++ docs/.vuepress/components/buttons.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h4>基本用法</h4>
+    <u-button>默认按钮</u-button>
+    <u-button class="bg-yellow">默认按钮</u-button>
+    <u-button class="bg-green">默认按钮</u-button>
+    <u-button class="bg-cyan">默认按钮</u-button>
+    <u-button class="bg-red">默认按钮</u-button>
+
+    <h4>图标按钮</h4>
+    <u-button icon="favorite-filling" class="mr">图标按钮</u-button>
+    <u-button icon="comment-filling" class="bg-yellow mr">图标按钮</u-button>
+    <u-button icon="check-item-filling" class="bg-green mr">图标按钮</u-button>
+    <u-button icon="folder-filling" class="bg-cyan mr">图标按钮</u-button>
+    <u-button icon="warning-filling" class="bg-red">图标按钮</u-button>
+
+    <h4>按钮组</h4>
+    <u-button-group>
+      <u-button icon="favorite-filling">按钮组</u-button>
+      <u-button icon="comment-filling" class="bg-yellow">按钮组</u-button>
+      <u-button icon="check-item-filling" class="bg-green">按钮组</u-button>
+      <u-button icon="folder-filling" class="bg-cyan">按钮组</u-button>
+      <u-button icon="warning-filling" class="bg-red">按钮组</u-button>
+    </u-button-group>
+
+    <h4>特殊按钮</h4>
+    <u-button icon="loading" class="bg-cyan mr" loading>旋转按钮</u-button>
+    <u-button icon="dynamic-filling" class="bg-gray" iconPosition="right" loading>旋转按钮</u-button>
+  </div>
+</template>
+<script>
+import UButton from '../../../src/button/button'
+import UButtonGroup from '../../../src/button/button-group'
+
+export default {
+  components: { UButton, UButtonGroup },
+  data() {
+    return {
+      loading: true
+    }
+  }
+}
+</script>
