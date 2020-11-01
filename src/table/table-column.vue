8714dfe03209ea3d354622c6623f8e1a8c752fbd
shunyue1320 <1277952981@qq.com>
2020-11-01 20:55:57 +0800

    完成table组件

---
+++ src/table/table-column.vue
@@ -0,0 +1,27 @@
+<template>
+  <th class="u-title-body">{{ label }}</th>
+</template>
+
+<script>
+  export default {
+    name: "UTableColumn",
+    props: {
+      prop: {
+        type: String,
+        required: true
+      },
+      label: {
+        type: String,
+        required: true
+      },
+      width: {
+        type: String,
+        required: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/table/table.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="u-table">
+    <table border="1">
+      <colgroup>
+        <col v-for="attr in childrenProps" :key="attr.label" span="1" style="">
+      </colgroup>
+      <tr class="u-table-title">
+        <slot></slot>
+      </tr> 
+      <tr v-for="(row, index) in data" :key="index" class="u-table-body">
+        <td v-for="attr in childrenProps" :key="attr.label">{{ row[attr.prop] }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "UTable",
+    props: {
+      data: {
+        type: Array,
+        required: true
+      }
+    },
+    data() {
+      return {
+        childrenProps: []
+      }
+    },
+    mounted() {
+      const childrenProps = []
+      for (let node of this.$children) {
+        childrenProps.push(node.$props)
+      }
+      this.childrenProps = childrenProps
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
